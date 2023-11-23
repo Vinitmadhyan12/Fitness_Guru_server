f2f3bc7d783bb394aa055e383d72ac65c9e9fe81
unknown <vinit.madhyan@spit.ac.in>
2023-11-23 21:27:31 +0530

    seventh commit

--- app.js
+++ app.js
@@ -51,6 +51,20 @@ passport.deserializeUser(Name.deserializeUser());
 
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with the actual origin of your client application
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true);
+
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 app.get("/", (req, res) => {
     // res.json({ message: "hello i am vinit" })
     res.send("Hello this is Fitness Guru website")
